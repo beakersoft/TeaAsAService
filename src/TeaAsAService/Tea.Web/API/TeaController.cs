9a63876fd4f184dfe7f7314d0e9a183d5d908073
Luke Niland <luke.niland@cmacgroup.co.uk>
2019-08-11 22:24:01 +0100

    Starting to scaffold up the project

---
+++ src/TeaAsAService/Tea.Web/API/TeaController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Tea.Web.API
+{
+    [Route("api/[controller]")]
+    [ApiVersion("1.0")]
+    [ApiController]
+    public class TeaController : ControllerBase
+    {
+        [HttpPost]
+        public IActionResult Add()
+        {
+            //create a new id
+            return Ok();
+        }
+
+        [HttpPost]
+        public IActionResult Add(string id)
+        {
+
+            return Ok();
+        }
+
+
+        [HttpGet]
+        [Route("get/{id}")]
+        public IActionResult Get(string id)
+        {
+
+            return Ok();
+        }
+    }
+}
