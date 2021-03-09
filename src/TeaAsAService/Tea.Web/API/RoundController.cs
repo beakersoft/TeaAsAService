79810032531b3b17f51299812727ceeeefdb9f6b
Jonathan Gzz <jonathangzzben@gmail.com>
2021-03-08 19:23:34 -0600

    Commit all controller actions and user models

--- src/TeaAsAService/Tea.Web/API/RoundController.cs
+++ src/TeaAsAService/Tea.Web/API/RoundController.cs
@@ -25,71 +25,107 @@ namespace Tea.Web.API
             _roundService = roundService;
         }
 
+        /// <summary>
+        /// Creates a new round.
+        /// </summary>
+        /// <response code="202">Succesful RoundNew Request</response>
+        /// <response code="400">Invalid RoundNew Request</response>
         [HttpPost]
         [Route("new")]
+        [ProducesResponseType(typeof(RoundSummaryModel), StatusCodes.Status202Accepted)]
+        [ProducesResponseType(typeof(string), StatusCodes.Status400BadRequest)]
         public async Task<IActionResult> New([FromBody] RoundModel model)
         {
             if (!ModelState.IsValid)
-                return ReturnError(StatusCodes.Status400BadRequest, "Invalid round creation request", GetModelStateMessages());
-  
+                return ReturnError(StatusCodes.Status400BadRequest, "Invalid RoundNew Request", GetModelStateMessages());
+
             var round = await model.CreateRoundFromModel(_dataStore);
 
             if (!model.IsRoundValid)
-                return ReturnError(StatusCodes.Status400BadRequest, "Invalid round creation request", model.AllValidationMessages);
-            
+                return ReturnError(StatusCodes.Status400BadRequest, "Invalid RoundNew Request", model.AllValidationMessages);
+
             await _dataStore.CreateAsync(round);
 
             return Accepted(RoundSummaryModel.FromRound(round));
         }
 
+        /// <summary>
+        /// Edits a round.
+        /// </summary>
+        /// <response code="202">Succesful RoundEdit Request</response>
+        /// <response code="400">Invalid RoundEdit Request</response>
+        /// <response code="403">Forbidden RoundEdit Request</response>
+        /// <response code="404">Invalid RoundEdit Request</response>
         [HttpPut]
         [Route("edit")]
+        [ProducesResponseType(typeof(Round), StatusCodes.Status202Accepted)]
+        [ProducesResponseType(typeof(string), StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(typeof(string), StatusCodes.Status403Forbidden)]
+        [ProducesResponseType(typeof(string), StatusCodes.Status404NotFound)]
         public async Task<IActionResult> Edit([FromBody] RoundModel model)
         {
             if (!ModelState.IsValid)
-                return ReturnError(StatusCodes.Status400BadRequest, "Invalid round edit request", GetModelStateMessages());
+                return ReturnError(StatusCodes.Status400BadRequest, "Invalid RoundEdit Request", GetModelStateMessages());
 
             if (!model.Id.HasValue)
-                return ReturnError(StatusCodes.Status400BadRequest, "Invalid round edit request", "Please send a valid round id to edit a round");
+                return ReturnError(StatusCodes.Status400BadRequest, "Invalid RoundEdit Request", "Please send a valid round id to edit a round");
 
             var round = await _dataStore.GetAsync<Round>(model.Id.Value);
 
             if (round == null)
-                return ReturnError(StatusCodes.Status404NotFound, "Invalid round edit request", $"Round {model.Id} not found");
+                return ReturnError(StatusCodes.Status404NotFound, "Invalid RoundEdit Request", $"Round {model.Id} not found");
 
             if (!round.CanUpdateRound(User))
-                return ReturnError(StatusCodes.Status403Forbidden, "Invalid had round request", $"You are not allowed to update this round");
+                return ReturnError(StatusCodes.Status403Forbidden, "Forbidden RoundEdit Request", $"You are not allowed to edit this round");
 
             round = await model.UpdateRound(round, _dataStore);
-            
+
             if (!model.IsRoundValid || round == null)
-                return ReturnError(StatusCodes.Status400BadRequest, "Invalid round edit request", model.AllValidationMessages);
+                return ReturnError(StatusCodes.Status400BadRequest, "Invalid RoundEdit Request", model.AllValidationMessages);
 
             round = await _dataStore.UpdateAsync(round);
 
             return Accepted(RoundSummaryModel.FromRound(round));
         }
 
+        /// <summary>
+        /// Registers that a user has had a round.
+        /// </summary>
+        /// <response code="200">Succesful HadRound Request</response>
+        /// <response code="400">Invalid HadRound Request</response>
+        /// <response code="403">Forbidden HadRound Request</response>
+        /// <response code="404">Invalid HadRound Request</response>
+        [ProducesResponseType(typeof(Round), StatusCodes.Status200OK)]
+        [ProducesResponseType(typeof(string), StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(typeof(string), StatusCodes.Status403Forbidden)]
+        [ProducesResponseType(typeof(string), StatusCodes.Status404NotFound)]
         [HttpPost]
         [Route("hadround")]
         public async Task<IActionResult> HadRound([FromBody] HadRoundModel model)
         {
             if (!ModelState.IsValid)
-                return ReturnError(StatusCodes.Status400BadRequest, "Invalid had round request", GetModelStateMessages());
+                return ReturnError(StatusCodes.Status400BadRequest, "Invalid HadRound request", GetModelStateMessages());
 
             var round = await _dataStore.GetAsync<Round>(model.Id);
 
             if (round == null)
-                return ReturnError(StatusCodes.Status404NotFound, "Invalid had round request", $"Round {model.Id} not found");
-            
-            if(!round.CanUpdateRound(User))
-                return ReturnError(StatusCodes.Status403Forbidden, "Invalid had round request", $"You are not allowed to update this round");
+                return ReturnError(StatusCodes.Status404NotFound, "Invalid HadRound request", $"Round {model.Id} not found");
 
-            await _roundService.UpdateExistingRoundAsync(round, _dataStore,model.UserGettingRound,model.RoundNotes);
+            if (!round.CanUpdateRound(User))
+                return ReturnError(StatusCodes.Status403Forbidden, "Invalid HadRound request", $"You are not allowed to edit this round");
+
+            await _roundService.UpdateExistingRoundAsync(round, _dataStore, model.UserGettingRound, model.RoundNotes);
 
             return Ok(RoundSummaryModel.FromRound(round));
         }
 
+        /// <summary>
+        /// Gets a round by Id.
+        /// </summary>
+        /// <response code="200">Succesful GetRound Request</response>
+        /// <response code="403">Forbidden GetRound Request</response>
+        [ProducesResponseType(typeof(Round), StatusCodes.Status200OK)]
+        [ProducesResponseType(typeof(string), StatusCodes.Status403Forbidden)]
         [HttpGet]
         [Route("get/{id}")]
         public async Task<IActionResult> Get([FromRoute] Guid id)
@@ -97,7 +133,7 @@ namespace Tea.Web.API
             var round = await _dataStore.GetAsync<Round>(id);
 
             if (!round.CanUpdateRound(User))
-                return ReturnError(StatusCodes.Status403Forbidden, "Invalid had round request", "You are not to access this round");
+                return ReturnError(StatusCodes.Status403Forbidden, "Invalid GetRound request", "You are not to access this round");
 
             return Ok(RoundSummaryModel.FromRound(round));
         }
--- src/TeaAsAService/Tea.Web/API/UserController.cs
+++ src/TeaAsAService/Tea.Web/API/UserController.cs
@@ -27,49 +27,67 @@ namespace Tea.Web.API
             _passwordHasher = passwordHasher;
         }
 
+        /// <summary>
+        /// Creates a new user.
+        /// </summary>
+        /// <response code="200">Succesful CreateUser Request</response>
+        /// <response code="400">Invalid CreateUser Request</response>
         [HttpPost]
         [AllowAnonymous]
         [Route("createuser")]
+        [ProducesResponseType(typeof(User), StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
         public async Task<IActionResult> CreateUser([FromBody] CreateUserModel model)
         {
             if (!ModelState.IsValid)
-                return ReturnError(StatusCodes.Status400BadRequest, "Invalid Create User Request", GetModelStateMessages());
+                return ReturnError(StatusCodes.Status400BadRequest, "Invalid CreateUser Request", GetModelStateMessages());
 
             if (string.IsNullOrEmpty(model.LocalizedString))
             {
-                 model.LocalizedString = HttpContext.Request.GetTypedHeaders()
-                .AcceptLanguage.OrderByDescending(x=>x.Quality ?? 0.1).FirstOrDefault()?.Value.ToString()
-                ?? "en-GB";
-            }         
-            
+                model.LocalizedString = HttpContext.Request.GetTypedHeaders()
+               .AcceptLanguage.OrderByDescending(x => x.Quality ?? 0.1).FirstOrDefault()?.Value.ToString()
+               ?? "en-GB";
+            }
+
             var user = Core.Domain.User.CreateNewUser(model.LocalizedString, model.Firstname, model.Surname);
 
             if (!user.SetPassword(model.Password, _passwordHasher))
-                return ReturnError(StatusCodes.Status400BadRequest, "Invalid Create User Request", "Password does not meet complexity requirements");
+                return ReturnError(StatusCodes.Status400BadRequest, "Invalid CreateUser Request", "Password does not meet complexity requirements");
 
             if (!user.SetEmail(model.EmailAddress))
-                return ReturnError(StatusCodes.Status400BadRequest, "Invalid Create User Request", "Email Address is not valid");
+                return ReturnError(StatusCodes.Status400BadRequest, "Invalid CreateUser Request", "Email Address is not valid");
 
             user = await _dataStore.CreateAsync(user);
 
             return Ok(user);
         }
 
+        /// <summary>
+        /// Updates user's information. Must be authenticated.
+        /// </summary>
+        /// <response code="200">Succesful UpdateUser Request</response>
+        /// <response code="400">Invalid UpdateUser Request</response>
+        /// <response code="403">Invalid UpdateUser Request</response>
+        /// <response code="404">Invalid UpdateUser Request</response>
         [HttpPut]
         [Route("updateuser")]
+        [ProducesResponseType(typeof(User), StatusCodes.Status200OK)]
+        [ProducesResponseType(typeof(string), StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(typeof(string), StatusCodes.Status403Forbidden)]
+        [ProducesResponseType(typeof(string), StatusCodes.Status404NotFound)]
         public async Task<IActionResult> UpdateUser([FromBody] UserUpdateModel model)
         {
             if (!ModelState.IsValid)
-                return ReturnError(StatusCodes.Status400BadRequest, "Invalid Update User Request", GetModelStateMessages());
+                return ReturnError(StatusCodes.Status400BadRequest, "Invalid UpdateUser Request", GetModelStateMessages());
 
             var user = await _dataStore.GetUserBySimpleIdAsync(model.SimpleId);
             var userId = Guid.Parse(User.GetUserId());
 
             if (userId != user.Id)
-                return ReturnError(StatusCodes.Status403Forbidden, "Invalid Update User Request", $"Logged in user cant update {userId.ToString()}");
+                return ReturnError(StatusCodes.Status403Forbidden, "Invalid UpdateUser Request", $"Logged in user cant update {userId.ToString()}");
 
             if (user == null)
-               return ReturnError(StatusCodes.Status404NotFound, "Invalid Update User Request", $"User {model.SimpleId} not found");
+                return ReturnError(StatusCodes.Status404NotFound, "Invalid UpdateUser Request", $"User {model.SimpleId} not found");
 
             model.UpdateUserFromModel(user, _passwordHasher);
             await _dataStore.UpdateAsync(user);
@@ -77,14 +95,21 @@ namespace Tea.Web.API
             return Ok(user);
         }
 
+        /// <summary>
+        /// Gets authenticated user's information.
+        /// </summary>
+        /// <response code="200">Succesful GetUser Request</response>
+        /// <response code="404">Invalid GetUser Request</response>
         [HttpGet]
+        [ProducesResponseType(typeof(User), StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
         public async Task<IActionResult> Get()
         {
             var userId = Guid.Parse(User.GetUserId());
             var user = await _dataStore.GetAsync<User>(userId);
 
             if (user == null)
-                return ReturnError(StatusCodes.Status404NotFound, "Invalid Update User Request", $"User {userId.ToString()} not found");
+                return ReturnError(StatusCodes.Status404NotFound, "Invalid GetUser Request", $"User {userId.ToString()} not found");
 
             return Ok(user);
         }
